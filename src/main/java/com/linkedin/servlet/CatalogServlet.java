20a83c0f8dfff3de47aa036ec6c027ecd274de97
Thiago Emmerich <thiago.emmerich@hotmail.com>
2022-09-15 19:54:33 -0300

    Finished the third chapter - JSP

--- src/main/java/com/linkedin/servlet/CatalogServlet.java
+++ src/main/java/com/linkedin/servlet/CatalogServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.AsyncContext;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -16,7 +17,7 @@ import org.jboss.logging.Logger;
 /**
  * Servlet implementation class CatalogServlet
  */
-@WebServlet(urlPatterns = "/CatalogServlet", asyncSupported = true)
+@WebServlet(urlPatterns = "/CatalogServlet")
 public class CatalogServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOGGER = Logger.getLogger(CatalogServlet.class);
@@ -44,69 +45,19 @@ public class CatalogServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		AsyncContext asyncContext = request.startAsync();
-		
-		asyncContext.start(new Runnable() {
-			
-			@Override
-			public void run() {
-				try {
-					Thread.sleep(5000);
-					LOGGER.info("Response returned by: " + Thread.currentThread().getName());					
-					returnResponse(request, response);
-					asyncContext.complete();
-				} catch (InterruptedException | IOException e) {
-					LOGGER.error(e);
-				}
-				
-			}
-		});
-	
-		
-		LOGGER.info("Initial request thread: " + Thread.currentThread().getName());
-	}
 
-	private void returnResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String name = request.getParameter("name");
 		String manufacturer = request.getParameter("manufacturer");
 		String sku = request.getParameter("sku");
-		
+
 		response.setHeader("someHeader", "someHeaderValue");
 		response.addCookie(new Cookie("someCookie", "someCookieValue"));
-		
-		Catalog.addItem(new CatalogItem(name, manufacturer,sku));
-		
-		PrintWriter out = response.getWriter();
-	
-		out.println("<html>");
-		out.println("<head></head>");
-		out.println("<body>");
-		out.println("<table>");
-		for(CatalogItem item:Catalog.getItems()) {
-			out.println("<tr>");
-			out.println("<td>");
-			out.print(item.getName());
-			out.println("</td>");
-			out.println("</tr>");
-		}
-		out.println("</table>");
-		out.println("</body>");
-		out.println("</html>");
+
+		Catalog.addItem(new CatalogItem(name, manufacturer, sku));
+
+		request.setAttribute("items", Catalog.getItems());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+		dispatcher.forward(request, response);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
