ca9dc124a22037a98229d80b8c4c4269ed16eddc
Thiago Emmerich <thiago.emmerich@hotmail.com>
2022-09-18 18:08:48 -0300

    Finished the JSF chapter

--- hsport-catalog-jsf/src/main/java/com/linkedin/jsf/CatalogItemFormBean.java
+++ hsport-catalog-jsf/src/main/java/com/linkedin/jsf/CatalogItemFormBean.java
@@ -1,37 +1,56 @@
 package com.linkedin.jsf;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
-@RequestScoped
+import org.jboss.logging.Logger;
+
+@SessionScoped
 @Named("catalogItemFormBean")
-public class CatalogItemFormBean {
-	private String name;
-	private String manufacturer;
-	private String sku;
+public class CatalogItemFormBean implements Serializable {
 
-	public String getName() {
-		return name;
+	private static final Logger LOG = Logger.getLogger(CatalogItemFormBean.class);
+	private static final long serialVersionUID = 382061028856044729L;
+	private CatalogItem item = new CatalogItem();
+	private List<CatalogItem> items = new ArrayList<>();
+
+	public String addItem() {
+		long itemId = this.items.size() + 1;
+
+		LOG.info("Creating a new item coping the existing one");
+		CatalogItem newItem = this.item;
+
+		LOG.info("Now instanciating a new object to clean the object");
+		this.item = new CatalogItem();
+
+		LOG.info("Setting the id " + itemId + " to the new object");
+		newItem.setItemId(itemId);
+
+		LOG.info("Id set to the new object -> " + newItem.getItemId());
+		this.items.add(newItem);
+		LOG.info("Item added: " + newItem.toString());
+
+		return "list?faces-redirect=true";
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public CatalogItem getItem() {
+		return item;
 	}
 
-	public String getManufacturer() {
-		return manufacturer;
+	public void setItem(CatalogItem item) {
+		this.item = item;
 	}
 
-	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
+	public List<CatalogItem> getItems() {
+		return items;
 	}
 
-	public String getSku() {
-		return sku;
-	}
-
-	public void setSku(String sku) {
-		this.sku = sku;
+	public void setItems(List<CatalogItem> items) {
+		this.items = items;
 	}
 
 }
